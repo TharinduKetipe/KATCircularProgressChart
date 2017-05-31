831a9ce6f11dad2a6d4f6790f76bf3cb17cf579c
Tharindu Ketipearachchi <katramesh91@gmail.com>
2017-05-31 09:52:03 +0530

    initial commit

---
+++ Example/KATCircularProgressChart/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  KATCircularProgressChart
+//
+//  Created by TharinduKetipe on 05/30/2017.
+//  Copyright (c) 2017 TharinduKetipe. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ Example/Pods/Target Support Files/Pods-KATCircularProgressChart_Example/Pods-KATCircularProgressChart_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KATCircularProgressChart_Example : NSObject
+@end
+@implementation PodsDummy_Pods_KATCircularProgressChart_Example
+@end
